de3bf7f0d5a07685518b3161f3ee1d91e849d1fd
stpCollabr8nLstn <amrios2817@gmail.com>
2019-05-02 23:39:47 -0400

    add styled-components

---
+++ src/components/Airplane.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Airplane = styled.div`
+ background: pink;
+`;
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+export const color = {
+    primary: '#7C2A8B',
+    secondary: '#E83597',
+    accent01: '#1A60E8',
+    accent02: '#F7C429',
+    white: '#FFF',
+    lightGray: '#A5A6A7',
+    textGray: '#48494A'
+}
